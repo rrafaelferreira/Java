1939d99c4a13f951791dc0ab4038952e504b0c05
rrafaelferreira <155680116+rrafaelferreira@users.noreply.github.com>
2024-09-17 18:29:10 -0300

    Serviço para listar produtos “Pare o processo”

--- aluno/src/main/java/br/com/avalicao/aluno/servico/AlunoServico.java
+++ aluno/src/main/java/br/com/avalicao/aluno/servico/AlunoServico.java
@@ -1,5 +1,19 @@
 package br.com.avalicao.aluno.servico;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.avalicao.aluno.modelo.AlunoModelo;
+import br.com.avalicao.aluno.repositorio.AlunoRepositorio;
+
+@Service
 public class AlunoServico {
-    
+    @Autowired
+    private AlunoRepositorio pr;
+
+    //Metodo para listar os alunos 
+
+    public Iterable<AlunoModelo> listar(){
+        return pr.findAll();
+    }
 }
