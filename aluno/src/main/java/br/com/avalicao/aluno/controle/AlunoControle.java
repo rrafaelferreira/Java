391ef0edbbeff636ff1336cedb8b0190dbeefeae
rrafaelferreira <155680116+rrafaelferreira@users.noreply.github.com>
2024-09-17 18:59:48 -0300

    Rota para cadastrar produtos Acessa ProdutoControle

--- aluno/src/main/java/br/com/avalicao/aluno/controle/AlunoControle.java
+++ aluno/src/main/java/br/com/avalicao/aluno/controle/AlunoControle.java
@@ -1,17 +1,28 @@
 package br.com.avalicao.aluno.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.avalicao.aluno.modelo.AlunoModelo;
 import br.com.avalicao.aluno.servico.AlunoServico;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
 public class AlunoControle {
     @Autowired
     private AlunoServico ps;
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?>cadastrar(@RequestBody AlunoModelo pm){
+        return ps.cadastrar(pm);
+    }
+    
+
 
     @GetMapping("/listar")
     public Iterable< AlunoModelo> listar(){
