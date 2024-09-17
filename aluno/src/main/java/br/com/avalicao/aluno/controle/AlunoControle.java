9341e01ff7ed291d24333d2617827eb76d4a02c3
rrafaelferreira <155680116+rrafaelferreira@users.noreply.github.com>
2024-09-17 18:09:55 -0300

    Configurando controle

--- aluno/src/main/java/br/com/avalicao/aluno/controle/AlunoControle.java
+++ aluno/src/main/java/br/com/avalicao/aluno/controle/AlunoControle.java
@@ -1,5 +1,13 @@
 package br.com.avalicao.aluno.controle;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;;
+
+@RestController
 public class AlunoControle {
     
+    @GetMapping("")
+    public String rota(){
+        return "api de alunos!";
+    }
 }
