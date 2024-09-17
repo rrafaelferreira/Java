1227f03893c4ca8853553c01382a72816d7183aa
rrafaelferreira <155680116+rrafaelferreira@users.noreply.github.com>
2024-09-17 18:37:41 -0300

    Rota para listar produtos Clica em ProdutoControle

--- aluno/src/main/java/br/com/avalicao/aluno/controle/AlunoControle.java
+++ aluno/src/main/java/br/com/avalicao/aluno/controle/AlunoControle.java
@@ -1,10 +1,24 @@
 package br.com.avalicao.aluno.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.avalicao.aluno.modelo.AlunoModelo;
+import br.com.avalicao.aluno.servico.AlunoServico;
+
 
 @RestController
 public class AlunoControle {
+    @Autowired
+    private AlunoServico ps;
+
+    @GetMapping("/listar")
+    public Iterable< AlunoModelo> listar(){
+        return ps.listar();
+    }
+    
+
     
     @GetMapping("")
     public String rota(){
